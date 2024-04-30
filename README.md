684023e6dce347ef1bbcded85106313f36dcf5af
h-r-wells2 <hrwells10246@gmail.com>
2024-04-30 16:51:18 +0530

    format code

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Hora
+
 Hora is a React Native mobile app built using Expo. This repository is only for recording progress while learning React Native.
--- context/GlobalProvider.js
+++ context/GlobalProvider.js
@@ -2,45 +2,45 @@ import { createContext, useContext, useState, useEffect } from "react";
 import { getCurrentUSer } from "../lib/appwrite";
 
 const GlobalContext = createContext()
-export const useGlobalContext = ()=>useContext(GlobalContext)
+export const useGlobalContext = () => useContext(GlobalContext)
 
 
-const GLobalProvider = ({children}) =>{
+const GLobalProvider = ({ children }) => {
 
-    const [isLoggedIn, setIsLoggedIn] = useState(false)
-    const [user, setUser] = useState(null)
-    const [isLoading, setIsLoading] = useState(true)
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [user, setUser] = useState(null)
+  const [isLoading, setIsLoading] = useState(true)
 
-    useEffect(() => {
-      getCurrentUSer().then((res)=>{
-        if(res){
-            setIsLoggedIn(true);
-            setUser(res);
-        }else{
-            setIsLoading(false)
-            setUser(null)
-        }
-      }).catch((error)=>{
-        console.log(error)
-      }).finally(()=>{
+  useEffect(() => {
+    getCurrentUSer().then((res) => {
+      if (res) {
+        setIsLoggedIn(true);
+        setUser(res);
+      } else {
         setIsLoading(false)
-      })
-    }, [])
-    
+        setUser(null)
+      }
+    }).catch((error) => {
+      console.log(error)
+    }).finally(() => {
+      setIsLoading(false)
+    })
+  }, [])
 
-    return(
-        <GlobalContext.Provider 
-        value={{
-            isLoggedIn,
-            setIsLoggedIn,
-            user,
-            setUser,
-            isLoading
-        }}
-        >
-            {children}
-        </GlobalContext.Provider>
-    )
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        isLoggedIn,
+        setIsLoggedIn,
+        user,
+        setUser,
+        isLoading
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  )
 }
 
 
